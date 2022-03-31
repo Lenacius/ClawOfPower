59d6e7333bd1ce715a9989f9b6788ad075c5994d
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2022-03-31 20:25:12 -0300

    Added debugger for raw values

--- Scripts/HandController.cs
+++ Scripts/HandController.cs
@@ -25,9 +25,11 @@ public class HandController : MonoBehaviour
 
     // Finger bend processing
     float[] fingerBend = { 0, 0, 0, 0, 0 };
-    void ProcessFingers(string[] rawValues) { 
-        for(int x = 1; x < 6; x++) // First value is the identifier FNG
-            fingerBend[x-1] = float.Parse(rawValues[x], CultureInfo.InvariantCulture);
+    void ProcessFingers(string[] rawValues) {
+        for (int x = 1; x < 6; x++) { // First value is the identifier FNG
+            fingerBend[x - 1] = float.Parse(rawValues[x], CultureInfo.InvariantCulture);
+            Debug.Log("Finger " + x + ':' + fingerBend[x - 1]);
+        }
     }
 
     // MPU6050 processing
@@ -37,6 +39,7 @@ public class HandController : MonoBehaviour
         rawGyro.x = float.Parse(rawValues[1], CultureInfo.InvariantCulture);
         rawGyro.y = float.Parse(rawValues[2], CultureInfo.InvariantCulture);
         rawGyro.z = float.Parse(rawValues[3], CultureInfo.InvariantCulture);
+        Debug.Log("Gyroscope values:" + rawGyro.x + '|' + rawGyro.y + '|' + rawGyro.z);
     }
 
     // Invoked when a connect/disconnect event occurs. The parameter 'success'
